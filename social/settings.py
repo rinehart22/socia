ce6c9339a6c95d4560f7f030c075af0a7ba8269f
rinehart22 <srikanthlegend9@gmail.com>
2022-03-04 17:10:58 +0530

    secnd

--- social/settings.py
+++ social/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/' 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-] 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ] 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
